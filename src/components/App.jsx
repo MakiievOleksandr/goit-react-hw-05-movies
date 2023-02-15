37c7c1d55da5affd99270a3fb629e6abe4aa0d1c
MakiievOleksandr <alsermak@gmail.com>
2023-02-15 19:48:26 +0200

    add lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,30 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import Header from './module/Header/Header';
-import HomePage from 'pages/HomePage/HomePage';
-import MoviesPage from 'pages/MoviePage/MoviesPage';
-import MovieDetailsPage from 'pages/MovieDetailsPage/MovieDetailsPage';
-import Cast from './module/Cast/Cast';
-import Reviews from './module/Reviews/Reviews';
+const Header = lazy(() => import('./module/Header/Header'));
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('pages/MoviePage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('pages/MovieDetailsPage/MovieDetailsPage')
+);
+const Cast = lazy(() => import('./module/Cast/Cast'));
+const Reviews = lazy(() => import('./module/Reviews/Reviews'));
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Header />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<HomePage />} />
-      </Routes>
+      <Suspense fallback={<p>...Loading</p>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
